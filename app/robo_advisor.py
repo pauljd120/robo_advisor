37ee0114bb127fefade470e929499c46a01cd8ae
pauljd120 <46468834+pauljd120@users.noreply.github.com>
2019-02-21 20:28:04 -0500

    writing example data to csv file

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -3,6 +3,7 @@ import json
 import os
 import requests
 import datetime
+import csv
 
 load_dotenv() # loads environment variables set in a ".env" file, including the value of the ALPHAVANTAGE_API_KEY variable
 
@@ -35,12 +36,17 @@ latest_day = dates[0]
 latest_close = parsed_response["Time Series (Daily)"][latest_day]["4. close"]
 
 high_prices = []
+low_prices = []
 
 for date in dates:
     high_price = tsd[date]["2. high"]
     high_prices.append(float(high_price))
+    low_price = tsd[date]["3. low"]
+    low_prices.append(float(low_price))
+
 
 recent_high = max(high_prices)
+recent_low = min(low_prices)
 
 
 
@@ -86,9 +92,23 @@ print("RUN AT: " + current_time.strftime("%x") + " " + current_time.strftime("%X
 print("-----------------")
 print(f"LATEST DAY OF AVAILABLE DATA: {last_refreshed}")
 print(f"LATEST DAILY CLOSING PRICE: {to_usd(float(latest_close))}")
-print(f"RECENT HIGH: {recent_high}")
-print("RECENT LOW: $99,000.00")
+print(f"RECENT HIGH: {to_usd(float(recent_high))}")
+print(f"RECENT LOW: {to_usd(float(recent_low))}")
 print("-----------------")
 print("RECOMMENDATION: Buy!")
 print("RECOMMENDATION REASON: Because the latest closing price is within threshold XYZ etc., etc. and this fits within your risk tolerance etc., etc.")
+print("-----------------")
+
+csv_file_path = os.path.join(os.path.dirname(__file__), "..", "data", "prices.csv")
+
+
+
+with open(csv_file_path, "w") as csv_file:
+    writer = csv.DictWriter(csv_file, fieldnames =["city","name"])
+    writer.writeheader()
+    writer.writerow({"city":"New York", "name":"Yankees"})
+
+print(f"WRITING DATA TO CSV: {csv_file_path}...")
+print("-----------------")
+print("HAPPY INVESTING!")
 print("-----------------")
