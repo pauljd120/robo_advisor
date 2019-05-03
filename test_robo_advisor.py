ce1cb72b7e0667aaef664c49b0d27e7b1b658d9e
pauljd120 <46468834+pauljd120@users.noreply.github.com>
2019-05-03 19:17:24 -0400

    Update test_robo_advisor.py

--- test_robo_advisor.py
+++ test_robo_advisor.py
@@ -1,1 +1,24 @@
-#test robo advisor
+from robo_advisor import to_usd, get_response
+
+import pytest
+import requests
+import dotenv
+import json
+
+def test_to_usd():
+    result = to_usd(50)
+    assert result == "$50.00"
+
+def test_get_response():
+
+    symbol = "MSFT"
+
+    request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={api_key}"
+
+    response = get_response(request_url)
+
+    parsed_response = json.loads(response.text)
+
+    list_parsed_response = list(parsed_response)
+
+    assert list_parsed_response[0] == "Meta Data"
